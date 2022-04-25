19ae07c442d0015944c04e227d07d14660092174
xurb <xurongbiao@hmtim.com>
2022-04-26 00:07:57 +0800

    自动注入名称注意要点

--- src/main/java/com/mashibing/controller/PersonController.java
+++ src/main/java/com/mashibing/controller/PersonController.java
@@ -4,6 +4,7 @@ package com.mashibing.controller;
 import com.mashibing.service.PersonService;
 import com.mashibing.service.PersonService2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
@@ -17,8 +18,11 @@ public class PersonController {
      *      如果找到直接赋值，找不到直接报错
      *      如果有多个类型一样的bean类对象，此时会按照id来进行查找，默认的id是类名的首字母小写
      *      如果找到直接注入，找不到直接报错
+     *
+     *      如果想要通过名字进行查找，可以自己规定名称，使用@Qualifier("personService")注解
      */
     @Autowired
+    @Qualifier("personService")
     private PersonService personService2;
 
     public void save(){
