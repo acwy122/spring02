bc50d7c7a510acdc7db6f7398611a0fd9d463ecb
xurb <xurongbiao@hmtim.com>
2022-04-25 23:56:22 +0800

    自动装配补充

--- src/main/java/com/mashibing/controller/PersonController.java
+++ src/main/java/com/mashibing/controller/PersonController.java
@@ -2,14 +2,21 @@
 package com.mashibing.controller;
 
 import com.mashibing.service.PersonService;
+import com.mashibing.service.PersonService2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PersonController {
+    /**
+     * 通过@AutoWired注解能够完成自动注入的功能
+     *      是按照什么方式进行自动注入的呢？
+     *
+     * 默认情况是按照类型byType来进行装配的
+     */
     @Autowired
-    private PersonService personService;
+    private PersonService2 personService;
 
     public void save(){
         personService.save();
--- src/main/java/com/mashibing/service/PersonService2.java
+++ src/main/java/com/mashibing/service/PersonService2.java
@@ -5,13 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PersonService {
+public class PersonService2 {
+
     @Autowired
     private PersonDao personDao;
 
     public void save(){
-        System.out.println("personService");
+        System.out.println("personService2.......");
         personDao.save();
     }
-
 }
